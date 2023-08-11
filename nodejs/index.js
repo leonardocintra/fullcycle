e1ef83e6508a8f412d19b37a97d523cd4d63eb64
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-11 13:53:25 -0300

    finalizado docker

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
-
 const app = express();
-
 const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const mysql = require("mysql2");
+const conn = mysql.createConnection(config);
+
+conn.connect();
+const sql = `INSERT INTO people (name) values ('Leonardo')`;
+conn.query(sql);
+conn.end();
 
 app.get("/", (req, res) => {
   res.send("Welcome ronaldinho");
