3ad2c619814908a4a582f993ca92ffd4c305e946
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-15 17:23:24 -0300

    ✨ feat(liveness):
    implementado

--- aula-kubernetes/index.js
+++ aula-kubernetes/index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const nome1 = process.env.NOME_1;
-const nome2 = process.env.NOME_2;
+let startedAt = Date.now();
 
 app.get("/", (req, res) => {
-  res.send(`Ola meu senhores ${nome1} e ${nome2}`);
+  res.send(`Ola meu senhores Leonardo`);
+});
+
+app.get("/health", (req, res) => {
+  // Verifica se já se passaram 30 segundos desde o startedAt
+  if (Date.now() - startedAt >= 30000) {
+    res.status(500).send("Internal Server Error - Deu certooo");
+  } else {
+    res.status(200).send("OK");
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// #F0101
