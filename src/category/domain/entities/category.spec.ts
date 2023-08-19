9207f4ecbb5f4c94b3b25d3a722d3aec5d68eed1
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-18 23:28:44 -0300

    ✨ feat:
    entity categories

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,6 +1,6 @@
 import { Category } from "./category"
 
-describe('Category Tests', () => {
+describe('Category Unit Tests', () => {
   test('constructor of category tests', () => {
     const category: Category = new Category('Terror')
     expect(category.name).toBe('Terror')
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,26 @@
+export type CategoryProperties = {
+  name: string,
+  description?: string,
+  is_active?: boolean,
+  created_at?: Date
+}
+
 export class Category {
-  constructor(public name: string) { }
+  constructor(readonly props: CategoryProperties) { }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get description(): string {
+    return this.description;
+  }
+
+  get is_active(): boolean | undefined {
+    return this.is_active;
+  }
+
+  get created_at(): Date {
+    return this.created_at;
+  }
 }
