18f274c61334d748d2f288eccc2698affe10b805
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-29 17:26:00 -0300

    🧪 test:
    add test

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -1,3 +1,4 @@
+import { validate as uuidValidate } from "uuid";
 import InvalidUuidError from "../errors/invalid-uuid.error";
 import UniqueEntityId from "./unique-entity-id.vo";
 
@@ -8,6 +9,21 @@ describe("UniqueEntityId Unit Tests", () => {
     expect(() => new UniqueEntityId("fake uuid")).toThrow(
       new InvalidUuidError()
     );
+    expect(validateSpy).toBeCalledTimes(1);
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const uuid = "11e551c0-862f-4570-8120-2dc316a2945d";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toBeCalledTimes(1);
+  });
+
+  it("should generate uuid", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const newUuid = new UniqueEntityId();
+    expect(uuidValidate(newUuid.id)).toBeTruthy();
     expect(validateSpy).toBeCalledTimes(1);
   });
 });
