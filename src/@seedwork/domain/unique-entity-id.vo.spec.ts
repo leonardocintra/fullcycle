967eb3826c0d3d6e8388fe6f0abc08fca0d5a032
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-29 17:37:58 -0300

    🦄 refactor:
    refatorando test

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -3,9 +3,9 @@ import InvalidUuidError from "../errors/invalid-uuid.error";
 import UniqueEntityId from "./unique-entity-id.vo";
 
 describe("UniqueEntityId Unit Tests", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+
   it("should throw when uuid is invalid", () => {
-    // tive que colocar "as any" pois o validate é privado
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     expect(() => new UniqueEntityId("fake uuid")).toThrow(
       new InvalidUuidError()
     );
@@ -13,7 +13,6 @@ describe("UniqueEntityId Unit Tests", () => {
   });
 
   it("should accept a uuid passed in constructor", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     const uuid = "11e551c0-862f-4570-8120-2dc316a2945d";
     const vo = new UniqueEntityId(uuid);
     expect(vo.id).toBe(uuid);
@@ -21,7 +20,6 @@ describe("UniqueEntityId Unit Tests", () => {
   });
 
   it("should generate uuid", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     const newUuid = new UniqueEntityId();
     expect(uuidValidate(newUuid.id)).toBeTruthy();
     expect(validateSpy).toBeCalledTimes(1);
